e5937d309169f36c3aadb837bb327b7bc9459b5a
JeduSan <jedbenedictlba@gmail.com>
2024-08-19 11:06:53 +0800

    Minor addition

---
+++ resources/views/landing.blade.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>{{config('app.name', 'Laravel')}}</title>
+</head>
+<body>
+    <nav>
+        <a href="/dashboard">Dashboard</a>
+        <a href="/login">Login</a>
+        <a href="/register">Register (Admission)</a>
+    </nav>
+    <h1>Landing Page</h1>
+</body>
+</html>
