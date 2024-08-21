1b6eee7203eb4af350cb3e674f94ce197e39f2fc
JeduSan <jedbenedictlba@gmail.com>
2024-08-21 22:17:10 +0800

    Styled ph address selector

--- resources/views/admission/partials/accordion/student_info.blade.php
+++ resources/views/admission/partials/accordion/student_info.blade.php
@@ -45,36 +45,29 @@
 
 
     {{-- ADDRESS SELECTOR --}}
-    <div class="col-sm-6">
-        <h3>Address Selector - Philippines</h3>
+    <div class="flex flex-col mb-3 col-sm-6">
+        <label class="form-label input-label">Region</label>
+        <select name="region" class="form-control form-control-md input-field-d" id="region"></select>
+        <input type="hidden" class="form-control form-control-md input-field-d" name="region" id="region-text" required>
     </div>
-    <hr>
-    <div class="mb-3 col-sm-6">
-        <label class="form-label">Region *</label>
-        <select name="region" class="form-control form-control-md" id="region"></select>
-        <input type="hidden" class="form-control form-control-md" name="region_text" id="region-text" required>
+    <div class="flex flex-col mb-3 col-sm-6">
+        <label class="form-label input-label">Province</label>
+        <select name="province" class="form-control form-control-md input-field-d" id="province"></select>
+        <input type="hidden" class="form-control form-control-md input-field-d" name="province" id="province-text" required>
     </div>
-    <div class="mb-3 col-sm-6">
-        <label class="form-label">Province *</label>
-        <select name="province" class="form-control form-control-md" id="province"></select>
-        <input type="hidden" class="form-control form-control-md" name="province_text" id="province-text" required>
+    <div class="flex flex-col mb-3 col-sm-6">
+        <label class="form-label input-label">City / Municipality</label>
+        <select name="city" class="form-control form-control-md input-field-d" id="city"></select>
+        <input type="hidden" class="form-control form-control-md input-field-d" name="city" id="city-text" required>
     </div>
-    <div class="mb-3 col-sm-6">
-        <label class="form-label">City / Municipality *</label>
-        <select name="city" class="form-control form-control-md" id="city"></select>
-        <input type="hidden" class="form-control form-control-md" name="city_text" id="city-text" required>
+    <div class="flex flex-col mb-3 col-sm-6">
+        <label class="form-label input-label">Barangay</label>
+        <select name="barangay" class="form-control form-control-md input-field-d" id="barangay"></select>
+        <input type="hidden" class="form-control form-control-md input-field-d" name="barangay" id="barangay-text" required>
     </div>
-    <div class="mb-3 col-sm-6">
-        <label class="form-label">Barangay *</label>
-        <select name="barangay" class="form-control form-control-md" id="barangay"></select>
-        <input type="hidden" class="form-control form-control-md" name="barangay_text" id="barangay-text" required>
-    </div>
-    <div class="mb-3 col-md-6">
-        <label for="street-text" class="form-label">Street (Optional)</label>
-        <input type="text" class="form-control form-control-md" name="street" id="street-text">
-    </div>
-    <div class="col-md-6">
-        <input type="submit" class="btn btn-success" name="submit">
+    <div class="flex flex-col mb-3 col-md-6">
+        <label for="street-text" class="form-label input-label">Street (Optional)</label>
+        <input type="text" class="form-control form-control-md input-field" name="street" id="street-text">
     </div>
     {{-- !ADDRESS SELECTOR --}}
 
