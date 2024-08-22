f206978620469015fb6dc36f0085680b1ba67f83
JeduSan <jedbenedictlba@gmail.com>
2024-08-22 15:46:05 +0800

    Update credentials.blade.php

--- resources/views/admission/partials/accordion/credentials.blade.php
+++ resources/views/admission/partials/accordion/credentials.blade.php
@@ -1,3 +1,20 @@
 <div class="flex flex-col p-4 ">
-    {{-- CONTENT --}}
+
+    <label class="input-label" for="shs-hs-card">Original Copy of Senior/Junior High Card</label>
+    <input class="input-field-d" type="file" name="shs_hs_card" id="shs-hs-card">
+
+    <label class="input-label" for="good-moral">Original Copy of Good Moral</label>
+    <input class="input-field-d" type="file" name="good_moral" id="good-moral">
+
+    <label class="input-label" for="birth-cert">PSA/Birth Certificate (1 Photocopy)</label>
+    <input class="input-field-d" type="file" name="birth_cert" id="birth-cert">
+
+    <label class="input-label" for="2x2-pic">2x2 Picture (2pcs., White Background)</label>
+    <input class="input-field-d" type="file" name="2x2_pic" id="2x2-pic">
+
+    <label class="input-label" for="entrance-exam-res">Entrance Exam Result</label>
+    <input class="input-field-d" type="file" name="entrance_exam_res" id="entrance-exam-res">
+
+    <label class="input-label" for="phys-test-res">Physical Test Result</label>
+    <input class="input-field-d" type="file" name="phys_test_res" id="phys-test-res">
 </div>
