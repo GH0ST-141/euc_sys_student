ed0a4d9793de1f4d3eb482fc6c6b54cc4d952a8f
GH0ST-141 <151105998+GH0ST-141@users.noreply.github.com>
2024-09-02 17:00:35 +0800

    Add New Files
    Login, Privacy, and Procedures page are added.

---
+++ DITO_JAHRED/login-script.js
@@ -0,0 +1,14 @@
+function login() {
+  alert("Log in button clicked!");
+}
+
+function register() {
+  alert("Register button clicked!");
+}
+
+function openNav() {
+  document.getElementById("Sidenav").style.width = "250px";
+}
+function closeNav() {
+  document.getElementById("Sidenav").style.width = "0";
+}
